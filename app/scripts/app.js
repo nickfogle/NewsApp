bbdfc3c651caa6e998ac6df4201ea5087fdc40c0
nickfogle <nickfogle@gmail.com>
2014-06-22 23:22:39 -0400

    adding user authentication error handling

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,6 +26,10 @@ app.config(function ($routeProvider) {
          templateUrl: 'views/register.html',
          controller: 'AuthCtrl'
       })
+      .when('/login', {
+         templateUrl: 'views/login.html',
+         controller: 'AuthCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
