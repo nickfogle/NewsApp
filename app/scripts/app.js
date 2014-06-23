4a0c10d538888171cb36160108d5bad4262ebd99
nickfogle <nickfogle@gmail.com>
2014-06-23 00:38:00 -0400

    file update

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,41 +1,37 @@
 /* global app:true */
-
 'use strict';
 
 var app = angular.module('angNewsApp', [
-   'ngAnimate',
-   'ngCookies',
-   'ngResource',
-   'ngSanitize',
-   'ngRoute',
-   'ngTouch',
-   'firebase'
-]);
-
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute',
+    'firebase'
+  ])
+.constant('FIREBASE_URL', 'https://angularnewsapp.firebaseio.com/');
 app.config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/posts.html',
-        controller: 'PostsCtrl'
-      })
-      .when('/posts/:postId', {
-        templateUrl: 'views/showpost.html',
-        controller: 'PostViewCtrl'
-      })
-      .when('/register', {
-         templateUrl: 'views/register.html',
-         controller: 'AuthCtrl'
-      })
-      .when('/login', {
-         templateUrl: 'views/login.html',
-         controller: 'AuthCtrl'
-      })
-      .when('/users/:username', {
-         templateUrl: 'views/profile.html',
-         controller: 'ProfileCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-
-}).constant('FIREBASE_URL', 'https://angularnewsapp.firebaseio.com/');
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/posts.html',
+      controller: 'PostsCtrl'
+    })
+    .when('/posts/:postId', {
+      templateUrl: 'views/showpost.html',
+      controller: 'PostViewCtrl'
+    })
+    .when('/register', {
+      templateUrl: 'views/register.html',
+      controller: 'AuthCtrl'
+    })
+    .when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'AuthCtrl'
+    })
+    .when('/users/:username', {
+      templateUrl: 'views/profile.html',
+      controller: 'ProfileCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
