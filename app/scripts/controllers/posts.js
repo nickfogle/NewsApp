6e15093eba1b1919d26696a73529ac3b2c037c8e
nickfogle <nickfogle@gmail.com>
2014-06-25 00:34:46 -0400

    finally voting is functional

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,15 +1,38 @@
 /*jshint indent:4 */
 'use strict';
 
+app.controller('PostsCtrl', function ($scope, $location, Post) {
+  if ($location.path() === '/') {
+    $scope.posts = Post.all;
+  }
 
-app.controller('PostsCtrl', function($scope, $location, Post) {
-    if ($location.path() === '/') {
-        $scope.posts = Post.all;
+  $scope.post = {url: 'http://', title: ''};
+
+  $scope.deletePost = function(postId) {
+    Post.delete(postId);
+  };
+
+  $scope.upVotePost = function (postId, upVoted) {
+    if (upVoted) {
+      Post.clearVote(postId, upVoted);
+    } else {
+      Post.upVote(postId);
     }
+  };
 
-    $scope.post = {url: 'http://', title: ''};
+  $scope.downVotePost = function (postId, downVoted) {
+    if (downVoted) {
+      Post.clearVote(postId, !downVoted);
+    } else {
+      Post.downVote(postId);
+    }
+  };
 
-    $scope.deletePost = function (postId) {
-        Post.delete(postId);
-    };
+  $scope.upVoted = function (post) {
+    return Post.upVoted(post);
+  };
+
+  $scope.downVoted = function (post) {
+    return Post.downVoted(post);
+  };
 });
